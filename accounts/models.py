d13bbaca53106e2fca3789a3f0b0c5d391f32865
Dias, Luan <diasluan@amazon.com>
2021-05-23 13:29:16 -0300

    setup djoser for auth

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
+
 
 class UserAccountManager(BaseUserManager):
     def create_user(self, email, name, password=None):
         if not email:
-            raise ValueError('User must have an email address')
+            raise ValueError("User must have an email address")
 
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
@@ -19,15 +24,15 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=512, unique=True)
     name = models.CharField(max_length=128)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
 
     objects = UserAccountManager()
 
     def get_full_name(self):
         return self.name
-    
+
     def __str__(self):
-        return f'<User name={self.get_full_name} email={self.email}>'
+        return f"<User name={self.get_full_name()} email={self.email}>"
